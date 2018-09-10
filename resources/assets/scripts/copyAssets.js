e52eacf759f149234703c0f238db0f766f34a24e
Igor Scheller <igor.scheller@igorshp.de>
2018-09-10 22:34:27 +0200

    Moved scripts/copyAssets.js to resources folder and fixed quotes

--- resources/assets/scripts/copyAssets.js
+++ resources/assets/scripts/copyAssets.js
@@ -1,15 +1,15 @@
-const fs = require("fs");
-const path = require("path");
-const mkdirp = require("mkdirp");
+const fs = require('fs');
+const path = require('path');
+const mkdirp = require('mkdirp');
 
 const baseAssetPath = {
-  src: path.resolve("resources/assets"),
-  target: path.resolve("public/assets")
+  src: path.resolve('resources/assets'),
+  target: path.resolve('public/assets')
 };
 
 const emojiPath = {
-  src: baseAssetPath.src + "/emojis",
-  target: baseAssetPath.target + "/emojis"
+  src: baseAssetPath.src + '/emojis',
+  target: baseAssetPath.target + '/emojis'
 };
 
 mkdirp.sync(emojiPath.target);
