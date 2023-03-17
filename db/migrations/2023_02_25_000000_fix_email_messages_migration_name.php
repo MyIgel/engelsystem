06cefae0a9bdedadbef6f3859b08f64192e37f2d
Igor Scheller <igor.scheller@igorshp.de>
2023-03-17 16:08:43 +0100

    Migrations: Fix migration name

---
+++ db/migrations/2023_02_25_000000_fix_email_messages_migration_name.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Engelsystem\Migrations;
+
+use Engelsystem\Database\Migration\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class FixEmailMessagesMigrationName extends Migration
+{
+    /**
+     * Run the migration
+     */
+    public function up(): void
+    {
+        $this->schema->getConnection()
+            ->table('migrations')
+            ->where('migration', '2023_02_26_000000_AddEmailMessagesToUsersSettings')
+            ->update([
+                'migration' => '2023_02_26_000000_add_email_messages_to_users_settings',
+            ]);
+    }
+
+    // Down migration not needed when on same version
+}
